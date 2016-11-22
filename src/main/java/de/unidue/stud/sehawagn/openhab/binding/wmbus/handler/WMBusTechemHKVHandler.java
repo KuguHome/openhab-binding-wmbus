7d6ba6ef00ec9945e0047524ef8004f87904e534
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2016-11-22 08:37:31 +0100

    finally change using latest jmbus make displaying data readings work for the first time

--- src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/handler/WMBusTechemHKVHandler.java
+++ src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/handler/WMBusTechemHKVHandler.java
@@ -3,20 +3,20 @@ package de.unidue.stud.sehawagn.openhab.binding.wmbus.handler;
 import static de.unidue.stud.sehawagn.openhab.binding.wmbus.WMBusBindingConstants.*;
 
 import java.text.SimpleDateFormat;
-import java.util.Collection;
 import java.util.Set;
 
 import org.eclipse.smarthome.config.core.Configuration;
-import org.eclipse.smarthome.config.core.status.ConfigStatusMessage;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
-import org.eclipse.smarthome.core.thing.binding.ConfigStatusThingHandler;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.RefreshType;
 import org.openmuc.jmbus.TechemHKVMessage;
 import org.openmuc.jmbus.WMBusMessage;
 import org.slf4j.Logger;
@@ -24,7 +24,7 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Sets;
 
-public class WMBusTechemHKVHandler extends ConfigStatusThingHandler {
+public class WMBusTechemHKVHandler extends BaseThingHandler {
 
     public final static Set<ThingTypeUID> SUPPORTED_THING_TYPES = Sets.newHashSet(THING_TYPE_WMBUS_TECHEM_HKV);
     private final Logger logger = LoggerFactory.getLogger(WMBusTechemHKVHandler.class);
@@ -38,31 +38,11 @@ public class WMBusTechemHKVHandler extends ConfigStatusThingHandler {
     }
 
     @Override
-    public Collection<ConfigStatusMessage> getConfigStatus() {
-        return null;
-    }
-
-    @Override
     public void initialize() {
-        logger.debug("Initializing TechemHKV handler.");
-
-        Configuration config = getThing().getConfiguration();
-        deviceId = (String) config.get(PROPERTY_HKV_ID);
-        WMBusMessage deviceMessage = getDevice();
-        System.out.print(deviceId);
-        System.out.print(deviceMessage);
-        if (deviceMessage instanceof TechemHKVMessage) {
-            TechemHKVMessage techemDeviceMessage = (TechemHKVMessage) deviceMessage;
-
-            updateState(CHANNEL_ROOMTEMPERATURE, new DecimalType(techemDeviceMessage.getT1()));
-            updateState(CHANNEL_RADIATORTEMPERATURE, new DecimalType(techemDeviceMessage.getT2()));
-            updateState(CHANNEL_CURRENTREADING, new DecimalType(techemDeviceMessage.getCurVal()));
-            updateState(CHANNEL_LASTREADING, new DecimalType(techemDeviceMessage.getLastVal()));
-            updateState(CHANNEL_RECEPTION, new DecimalType(techemDeviceMessage.getRssi()));
-            updateState(CHANNEL_LASTDATE, new StringType(dateFormat.format(techemDeviceMessage.getLastDate())));
-            updateState(CHANNEL_CURRENTDATE, new StringType(dateFormat.format(techemDeviceMessage.getCurDate())));
-            updateState(CHANNEL_ALMANAC, new StringType(techemDeviceMessage.getHistory()));
-        }
+        logger.debug("Initializing WMBusTechemHKVHandler handler.");
+        Configuration config = getConfig();
+        deviceId = (String) config.getProperties().get(PROPERTY_HKV_ID);
+        updateStatus(ThingStatus.ONLINE);
     }
 
     @Override
@@ -72,7 +52,26 @@ public class WMBusTechemHKVHandler extends ConfigStatusThingHandler {
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        // no commands possible
+        if (command == RefreshType.REFRESH) {
+            WMBusMessage deviceMessage = getDevice();
+
+            if (deviceMessage instanceof TechemHKVMessage) {
+
+                TechemHKVMessage techemDeviceMessage = (TechemHKVMessage) deviceMessage;
+
+                updateState(CHANNEL_ROOMTEMPERATURE, new DecimalType(techemDeviceMessage.getT1()));
+                updateState(CHANNEL_RADIATORTEMPERATURE, new DecimalType(techemDeviceMessage.getT2()));
+                updateState(CHANNEL_CURRENTREADING, new DecimalType(techemDeviceMessage.getCurVal()));
+                updateState(CHANNEL_LASTREADING, new DecimalType(techemDeviceMessage.getLastVal()));
+                updateState(CHANNEL_RECEPTION, new DecimalType(techemDeviceMessage.getRssi()));
+                updateState(CHANNEL_LASTDATE,
+                        new StringType(dateFormat.format(techemDeviceMessage.getLastDate().getTime())));
+                updateState(CHANNEL_CURRENTDATE,
+                        new StringType(dateFormat.format(techemDeviceMessage.getCurDate().getTime())));
+
+                updateState(CHANNEL_ALMANAC, new StringType(techemDeviceMessage.getHistory()));
+            }
+        }
     }
 
     private synchronized WMBusBridgeHandler getBridgeHandler() {
