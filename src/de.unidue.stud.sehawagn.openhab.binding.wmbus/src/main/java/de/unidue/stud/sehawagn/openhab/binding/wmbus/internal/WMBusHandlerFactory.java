ec849245675749ff3d3b3da5c466cb9ed9478453
Roman Malyugin <roman.malyugin@altabel.com>
2018-08-30 13:40:07 +0200

    added device. eclipse doesn't generate xmls

--- src/de.unidue.stud.sehawagn.openhab.binding.wmbus/src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/internal/WMBusHandlerFactory.java
+++ src/de.unidue.stud.sehawagn.openhab.binding.wmbus/src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/internal/WMBusHandlerFactory.java
@@ -1,9 +1,11 @@
 package de.unidue.stud.sehawagn.openhab.binding.wmbus.internal;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.smarthome.config.discovery.DiscoveryService;
 import org.eclipse.smarthome.core.thing.Bridge;
@@ -13,83 +15,108 @@ import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Sets;
+import com.google.common.collect.ImmutableSet;
 
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.WMBusBindingConstants;
-import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.TechemHKVHandler;
-import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.WMBusBridgeHandler;
+import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.ADEUNISGasMeterHandler;
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.KamstrupMultiCal302Handler;
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.QundisQCaloricHandler;
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.QundisQHeatHandler;
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.QundisQWaterHandler;
+import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.TechemHKVHandler;
+import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.WMBusBridgeHandler;
 import de.unidue.stud.sehawagn.openhab.binding.wmbus.internal.discovery.WMBusDiscoveryService;
 
 /*
  * This class is the main entry point of the binding.
  */
+@Component(service = { WMBusHandlerFactory.class, BaseThingHandlerFactory.class })
 public class WMBusHandlerFactory extends BaseThingHandlerFactory {
 
-	// add new devices here
-	// TODO make this nicer instead of cascading
-	public final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Sets.union(WMBusBridgeHandler.SUPPORTED_THING_TYPES, Sets.union(TechemHKVHandler.SUPPORTED_THING_TYPES, Sets.union(QundisQCaloricHandler.SUPPORTED_THING_TYPES, Sets.union(QundisQWaterHandler.SUPPORTED_THING_TYPES, Sets.union(KamstrupMultiCal302Handler.SUPPORTED_THING_TYPES, QundisQHeatHandler.SUPPORTED_THING_TYPES)))));
+    private Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegs = new HashMap<>();
+    private Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS;
 
-	private Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegs = new HashMap<>();
+    // OpenHAB logger
+    private final Logger logger = LoggerFactory.getLogger(WMBusHandlerFactory.class);
 
-	// OpenHAB logger
-	private final Logger logger = LoggerFactory.getLogger(WMBusHandlerFactory.class);
+    public WMBusHandlerFactory() {
+        logger.debug("wmbus binding starting up.");
+    }
 
-	public WMBusHandlerFactory() {
-		logger.debug("wmbus binding starting up.");
-	}
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
 
-	@Override
-	public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-		return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
-	}
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-	@Override
-	protected ThingHandler createHandler(Thing thing) {
-		ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+        if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_BRIDGE)) {
+            // create handler for WMBus bridge
+            logger.debug("Creating (handler for) WMBus bridge.");
+            if (thing instanceof Bridge) {
+                WMBusBridgeHandler handler = new WMBusBridgeHandler((Bridge) thing);
+                registerDiscoveryService(handler);
+                return handler;
+            } else {
+                return null;
+            }
+            // add new devices here
+        } else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_TECHEM_HKV)) {
+            logger.debug("Creating (handler for) TechemHKV device.");
+            return new TechemHKVHandler(thing);
+        } else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QCALORIC_5_5)) {
+            logger.debug("Creating (handler for) Qundis Qcaloric 5,5 device.");
+            return new QundisQCaloricHandler(thing);
+        } else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QWATER_5_5)) {
+            logger.debug("Creating (handler for) Qundis Qwater 5,5 device.");
+            return new QundisQWaterHandler(thing);
+        } else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QHEAT_5)) {
+            logger.debug("Creating (handler for) Qundis Qheat 5 device.");
+            return new QundisQHeatHandler(thing);
+        } else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_KAMSTRUP_MULTICAL_302)) {
+            logger.debug("Creating (handler for) Kamstrup MultiCal 302 device.");
+            return new KamstrupMultiCal302Handler(thing);
+        } else if (thingTypeUID.equals(ADEUNISGasMeterHandler.THING_TYPE_ADEUNIS_GAS_METER_3)) {
+            logger.debug("Creating (handler for) ADEUNIS_RF Gas Meter (v.3) device.");
+            return new ADEUNISGasMeterHandler(thing);
+        } else {
+            return null;
+        }
+    }
 
-		if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_BRIDGE)) {
-			// create handler for WMBus bridge
-			logger.debug("Creating (handler for) WMBus bridge.");
-			if (thing instanceof Bridge) {
-				WMBusBridgeHandler handler = new WMBusBridgeHandler((Bridge) thing);
-				registerDiscoveryService(handler);
-				return handler;
-			} else {
-				return null;
-			}
-			// add new devices here
-		} else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_TECHEM_HKV)) {
-			logger.debug("Creating (handler for) TechemHKV device.");
-			return new TechemHKVHandler(thing);
-		} else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QCALORIC_5_5)) {
-			logger.debug("Creating (handler for) Qundis Qcaloric 5,5 device.");
-			return new QundisQCaloricHandler(thing);
-		} else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QWATER_5_5)) {
-			logger.debug("Creating (handler for) Qundis Qwater 5,5 device.");
-			return new QundisQWaterHandler(thing);
-		} else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_QUNDIS_QHEAT_5)) {
-			logger.debug("Creating (handler for) Qundis Qheat 5 device.");
-			return new QundisQHeatHandler(thing);
-		} else if (thingTypeUID.equals(WMBusBindingConstants.THING_TYPE_KAMSTRUP_MULTICAL_302)) {
-			logger.debug("Creating (handler for) Kamstrup MultiCal 302 device.");
-			return new KamstrupMultiCal302Handler(thing);
-		} else {
-			return null;
-		}
-	}
+    private synchronized void registerDiscoveryService(WMBusBridgeHandler bridgeHandler) {
+        logger.debug("Registering discovery service.");
+        WMBusDiscoveryService discoveryService = new WMBusDiscoveryService(bridgeHandler);
+        discoveryService.activate();
+        this.discoveryServiceRegs.put(bridgeHandler.getThing().getUID(), bundleContext
+                .registerService(DiscoveryService.class.getName(), discoveryService, new Hashtable<String, Object>()));
+    }
 
-	private synchronized void registerDiscoveryService(WMBusBridgeHandler bridgeHandler) {
-		logger.debug("Registering discovery service.");
-		WMBusDiscoveryService discoveryService = new WMBusDiscoveryService(bridgeHandler);
-		discoveryService.activate();
-		this.discoveryServiceRegs.put(bridgeHandler.getThing().getUID(), bundleContext.registerService(DiscoveryService.class.getName(), discoveryService, new Hashtable<String, Object>()));
-	}
+    @Activate
+    protected void activate(Map<String, String> properties) {
+        SUPPORTED_THING_TYPES_UIDS = ImmutableSet
+                .of(WMBusBridgeHandler.SUPPORTED_THING_TYPES, TechemHKVHandler.SUPPORTED_THING_TYPES,
+                        QundisQCaloricHandler.SUPPORTED_THING_TYPES, QundisQWaterHandler.SUPPORTED_THING_TYPES,
+                        QundisQHeatHandler.SUPPORTED_THING_TYPES, KamstrupMultiCal302Handler.SUPPORTED_THING_TYPES,
+                        adeunisGasMeterHandler.getSupportedThingTypes())
+                .stream().flatMap(Collection::stream).collect(Collectors.toSet());
+    }
 
+    @Deactivate
+    protected void deactivate() {
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.DYNAMIC)
+    volatile protected ADEUNISGasMeterHandler adeunisGasMeterHandler;
 }
--- src/de.unidue.stud.sehawagn.openhab.binding.wmbus/src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/internal/WMBusReceiver.java
+++ src/de.unidue.stud.sehawagn.openhab.binding.wmbus/src/main/java/de/unidue/stud/sehawagn/openhab/binding/wmbus/internal/WMBusReceiver.java
@@ -18,98 +18,108 @@ import de.unidue.stud.sehawagn.openhab.binding.wmbus.handler.WMBusBridgeHandler;
  */
 public class WMBusReceiver implements WMBusListener {
 
-	int[] filterIDs = new int[] {};
+    int[] filterIDs = new int[] {};
 
-	private WMBusBridgeHandler wmBusBridgeHandler;
+    private WMBusBridgeHandler wmBusBridgeHandler;
 
-	public static String VENDOR_TECHEM = "TCH";
-	public static String VENDOR_QUNDIS = "QDS";
-	public static String VENDOR_KAMSTRUP = "KAM";
+    public static String VENDOR_TECHEM = "TCH";
+    public static String VENDOR_QUNDIS = "QDS";
+    public static String VENDOR_KAMSTRUP = "KAM";
+    public static String VENDOR_ADEUNIS = "ARF";
 
-	private final Logger logger = LoggerFactory.getLogger(WMBusReceiver.class);
+    private final Logger logger = LoggerFactory.getLogger(WMBusReceiver.class);
 
-	public WMBusReceiver(WMBusBridgeHandler wmBusBridgeHandler) {
-		this.wmBusBridgeHandler = wmBusBridgeHandler;
-	}
+    public WMBusReceiver(WMBusBridgeHandler wmBusBridgeHandler) {
+        this.wmBusBridgeHandler = wmBusBridgeHandler;
+    }
 
-	public int[] getFilterIDs() {
-		logger.debug("getFilterIDs(): got {}", Arrays.toString(filterIDs));
-		return filterIDs;
-	}
+    public int[] getFilterIDs() {
+        logger.debug("getFilterIDs(): got {}", Arrays.toString(filterIDs));
+        return filterIDs;
+    }
 
-	public void setFilterIDs(int[] filterIDs) {
-		logger.debug("setFilterIDs() to {}", Arrays.toString(filterIDs));
-		this.filterIDs = filterIDs;
-	}
+    public void setFilterIDs(int[] filterIDs) {
+        logger.debug("setFilterIDs() to {}", Arrays.toString(filterIDs));
+        this.filterIDs = filterIDs;
+    }
 
-	boolean filterMatch(int inQuestion) {
-		logger.trace("filterMatch(): are we interested in device {}?", Integer.toString(inQuestion));
-		if (filterIDs.length == 0) {
-			logger.trace("filterMatch(): length is zero -> yes");
-			return true;
-		}
-		for (int i = 0; i < filterIDs.length; i++) {
-			if (filterIDs[i] == inQuestion) {
-				logger.debug("filterMatch(): found the device -> yes");
-				return true;
-			}
-		}
-		logger.trace("filterMatch(): not found");
-		return false;
-	}
+    boolean filterMatch(int inQuestion) {
+        logger.trace("filterMatch(): are we interested in device {}?", Integer.toString(inQuestion));
+        if (filterIDs.length == 0) {
+            logger.trace("filterMatch(): length is zero -> yes");
+            return true;
+        }
+        for (int i = 0; i < filterIDs.length; i++) {
+            if (filterIDs[i] == inQuestion) {
+                logger.debug("filterMatch(): found the device -> yes");
+                return true;
+            }
+        }
+        logger.trace("filterMatch(): not found");
+        return false;
+    }
 
-	/*
-	 * Handle incoming WMBus message from radio module.
-	 *
-	 * @see org.openmuc.jmbus.WMBusListener#newMessage(org.openmuc.jmbus.WMBusMessage)
-	 */
-	@Override
-	public void newMessage(WMBusMessage message) {
-		logger.trace("newMessage(): new message received");
-		WMBusDevice device = null;
-		if (filterMatch(message.getSecondaryAddress().getDeviceId().intValue())) {
-			logger.trace("newMessage(): Matched message received: {}", message.toString());
-			// print basic info
-			logger.trace("newMessage(): control field: {}, secondary address: {}", message.getControlField(), message.getSecondaryAddress().toString());
-			// decode VDR
-			VariableDataStructure vdr = message.getVariableDataResponse();
-			try {
-				vdr.decode();
-				logger.trace("newMessage(): variable data response decoded");
-				// VDR needs to be decoded for correct header information
-				logger.trace("newMessage(): access number: {}, status: {}, encryption mode: {}, number of encrypted blocks: {}", vdr.getAccessNumber(), vdr.getStatus(), vdr.getEncryptionMode(), vdr.getNumberOfEncryptedBlocks());
-				for (DataRecord record : vdr.getDataRecords()) {
-					logger.trace("> record: {}", record.toString());
-				}
-				device = new WMBusDevice(message);
-			} catch (DecodingException e) {
-				logger.trace("newMessage(): access number: {}, status: {}, encryption mode: {}, number of encrypted blocks: {}", vdr.getAccessNumber(), vdr.getStatus(), vdr.getEncryptionMode(), vdr.getNumberOfEncryptedBlocks());
-				logger.trace("newMessage(): could not decode as standard WMBus application layer message: {}", e.getMessage());
-				device = new TechemHKV(message);
-				try {
-					logger.trace("newMessage(): try to decode as Techem message");
-					device.decode();
-					wmBusBridgeHandler.processMessage(device);
-				} catch (DecodingException e1) {
-					logger.debug("newMessage(): could not decode as Techem Message: {}, original message: {}", e1.getMessage(), message.toString());
-				}
-			}
-			wmBusBridgeHandler.processMessage(device);
-			logger.trace("newMessage(): Forwarded to handler.processMessage()");
-		} else {
-			logger.trace("newMessage(): Unmatched message received: {}", message.toString());
-		}
-	}
+    /*
+     * Handle incoming WMBus message from radio module.
+     *
+     * @see org.openmuc.jmbus.WMBusListener#newMessage(org.openmuc.jmbus.WMBusMessage)
+     */
+    @Override
+    public void newMessage(WMBusMessage message) {
+        logger.trace("newMessage(): new message received");
+        WMBusDevice device = null;
+        if (filterMatch(message.getSecondaryAddress().getDeviceId().intValue())) {
+            logger.trace("newMessage(): Matched message received: {}", message.toString());
+            // print basic info
+            logger.trace("newMessage(): control field: {}, secondary address: {}", message.getControlField(),
+                    message.getSecondaryAddress().toString());
+            // decode VDR
+            VariableDataStructure vdr = message.getVariableDataResponse();
+            try {
+                vdr.decode();
+                logger.trace("newMessage(): variable data response decoded");
+                // VDR needs to be decoded for correct header information
+                logger.trace(
+                        "newMessage(): access number: {}, status: {}, encryption mode: {}, number of encrypted blocks: {}",
+                        vdr.getAccessNumber(), vdr.getStatus(), vdr.getEncryptionMode(),
+                        vdr.getNumberOfEncryptedBlocks());
+                for (DataRecord record : vdr.getDataRecords()) {
+                    logger.trace("> record: {}", record.toString());
+                }
+                device = new WMBusDevice(message);
+            } catch (DecodingException e) {
+                logger.trace(
+                        "newMessage(): access number: {}, status: {}, encryption mode: {}, number of encrypted blocks: {}",
+                        vdr.getAccessNumber(), vdr.getStatus(), vdr.getEncryptionMode(),
+                        vdr.getNumberOfEncryptedBlocks());
+                logger.trace("newMessage(): could not decode as standard WMBus application layer message: {}",
+                        e.getMessage());
+                device = new TechemHKV(message);
+                try {
+                    logger.trace("newMessage(): try to decode as Techem message");
+                    device.decode();
+                    wmBusBridgeHandler.processMessage(device);
+                } catch (DecodingException e1) {
+                    logger.debug("newMessage(): could not decode as Techem Message: {}, original message: {}",
+                            e1.getMessage(), message.toString());
+                }
+            }
+            wmBusBridgeHandler.processMessage(device);
+            logger.trace("newMessage(): Forwarded to handler.processMessage()");
+        } else {
+            logger.trace("newMessage(): Unmatched message received: {}", message.toString());
+        }
+    }
 
-	@Override
-	public void discardedBytes(byte[] bytes) {
-		logger.debug("Bytes discarded by radio module: {}", HexConverter.bytesToHex(bytes));
-	}
+    @Override
+    public void discardedBytes(byte[] bytes) {
+        logger.debug("Bytes discarded by radio module: {}", HexConverter.bytesToHex(bytes));
+    }
 
-	@Override
-	public void stoppedListening(IOException e) {
-		logger.debug("Stopped listening for new messages. Reason: {} {}", e.getClass().getSimpleName(), e.getMessage());
-		e.printStackTrace();
-	}
+    @Override
+    public void stoppedListening(IOException e) {
+        logger.debug("Stopped listening for new messages. Reason: {} {}", e.getClass().getSimpleName(), e.getMessage());
+        e.printStackTrace();
+    }
 
 }
