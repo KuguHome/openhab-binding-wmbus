e0a3f6fef2427958be84bcfed89328c78d0b9ac8
Łukasz Dywicki <luke@code-house.org>
2018-11-13 11:49:44 +0100

    Basic testing tools allowing to collect and replay frames.

---
+++ org.openhab.binding.wmbus.tools/src/main/java/org/openhab/binding/wmbus/tools/CollectorServlet.java
@@ -0,0 +1,130 @@
+package org.openhab.binding.wmbus.tools;
+
+import com.google.common.collect.EvictingQueue;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.smarthome.core.util.HexUtils;
+import org.openhab.binding.wmbus.WMBusDevice;
+import org.openhab.binding.wmbus.handler.WMBusAdapter;
+import org.openhab.binding.wmbus.handler.WMBusMessageListener;
+import org.openmuc.jmbus.SecondaryAddress;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+
+@Component
+public class CollectorServlet extends HttpServlet implements WMBusMessageListener {
+
+    private HttpService httpService;
+
+    // keep insertion order
+    private Map<SecondaryAddress, EvictingQueue<Entry>> entries = Collections.synchronizedMap(new LinkedHashMap<>());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String table = IOUtils.toString(getClass().getResourceAsStream("/table.html"));
+
+        table = table.replace("__rows__", getRows());
+
+
+        Optional<String> frameCount = entries.values().stream().map(Queue::size).reduce(Integer::sum).map(Object::toString);
+        table = table.replace("__devices__", "" + entries.size());
+        table = table.replace("__frames__", frameCount.orElse("0"));
+
+        IOUtils.write(table, resp.getOutputStream());
+        resp.getOutputStream().flush();
+    }
+
+    private String getRows() throws IOException {
+        String rows = "";
+        String template = IOUtils.toString(getClass().getResourceAsStream("/row.html"));
+
+        Set<SecondaryAddress> addresses = entries.keySet();
+        for (SecondaryAddress address : addresses) {
+            String row = template.replace("__ID__", address.getDeviceId().toString());
+            row = row.replace("__Manufacturer__", address.getManufacturerId());
+            row = row.replace("__Type__", address.getDeviceType().name());
+            row = row.replace("__Version__", "" + address.getVersion());
+            row = row.replace("__Frame__", getFrames(entries.get(address)));
+
+            rows += row;
+        }
+
+        return rows;
+    }
+
+    private String getFrames(EvictingQueue<Entry> entries) {
+        String list = "<ul>";
+
+        for (Entry entry : entries) {
+            LocalDateTime dateTime = Instant.ofEpochMilli(entry.time).atZone(ZoneId.systemDefault()).toLocalDateTime();
+            list += "<li>" + dateTime + "<br />" + HexUtils.bytesToHex(entry.frame) + "</li>";
+        }
+
+        return list + "</ul>";
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Reference
+    public void setHttpService(HttpService httpService) {
+        this.httpService = httpService;
+    }
+
+    @Activate
+    public void activate() throws ServletException, NamespaceException {
+        httpService.registerServlet("/wmbus/collector", this, new Hashtable(), null);
+    }
+
+    @Deactivate
+    public void deactivate() {
+        httpService.unregister("/wmbus/collector");
+    }
+
+    @Override
+    public void onNewWMBusDevice(WMBusAdapter wmBusAdapter, WMBusDevice wmBusDevice) {
+        push(wmBusDevice);
+    }
+
+    @Override
+    public void onChangedWMBusDevice(WMBusAdapter wmBusAdapter, WMBusDevice wmBusDevice) {
+        push(wmBusDevice);
+    }
+
+    private void push(WMBusDevice device) {
+        SecondaryAddress address = device.getOriginalMessage().getSecondaryAddress();
+
+        if (!entries.containsKey(address)) {
+            entries.put(address, EvictingQueue.create(2000));
+        }
+
+        Entry entry = new Entry(System.currentTimeMillis(), device.getOriginalMessage().asBlob());
+        entries.get(address).add(entry);
+    }
+
+    static class Entry {
+
+        public final long time;
+        public final byte[] frame;
+
+        public Entry(long time, byte[] frame) {
+            this.time = time;
+            this.frame = frame;
+        }
+    }
+}
---
+++ org.openhab.binding.wmbus.tools/src/main/java/org/openhab/binding/wmbus/tools/InjectorServlet.java
@@ -0,0 +1,123 @@
+package org.openhab.binding.wmbus.tools;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingManager;
+import org.eclipse.smarthome.core.thing.ThingRegistry;
+import org.eclipse.smarthome.core.util.HexUtils;
+import org.openhab.binding.wmbus.WMBusBindingConstants;
+import org.openhab.binding.wmbus.handler.WMBusAdapter;
+import org.openhab.binding.wmbus.WMBusDevice;
+import org.openmuc.jmbus.DecodingException;
+import org.openmuc.jmbus.wireless.VirtualWMBusMessageHelper;
+import org.openmuc.jmbus.wireless.WMBusMessage;
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class InjectorServlet extends HttpServlet {
+
+    private ThingRegistry thingRegistry;
+    private HttpService httpService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String form = IOUtils.toString(getClass().getResourceAsStream("/form.html"));
+
+        form = form.replace("__adapters__", getAdapters());
+        form = form.replace("__error__", getError(req));
+
+        IOUtils.write(form, resp.getOutputStream());
+        resp.getOutputStream().flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String adapterId = req.getParameter("adapter");
+        String frame = req.getParameter("frame");
+
+        if (adapterId == null || frame == null || adapterId.trim().isEmpty() || frame.trim().isEmpty()) {
+            doGet(req, resp);
+            return;
+        }
+
+        Optional<Thing> wmBusAdapter = adapters().stream()
+            .filter(adapter -> adapterId.equals(adapter.getUID().toString()))
+            .findFirst();
+
+        if (wmBusAdapter.isPresent()) {
+            inject((WMBusAdapter) wmBusAdapter.get().getHandler(), frame, req, resp);
+        }
+
+        doGet(req, resp);
+    }
+
+    private String getError(HttpServletRequest req) {
+        return Optional.ofNullable(req.getAttribute("error"))
+                .map(e -> (Throwable) e)
+                .map(e -> render(e))
+                .orElse("");
+    }
+
+    private String render(Throwable error) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(2048);
+        error.printStackTrace(new PrintWriter(bos));
+        return "<pre>" + error.getMessage() + "\n" + new String(bos.toByteArray()) + "</pre>";
+    }
+
+    private Collection<Thing> adapters() {
+        return thingRegistry.getAll().stream()
+            .filter(thing -> WMBusBindingConstants.BINDING_ID.equals(thing.getUID().getBindingId()))
+            .filter(thing -> thing.getHandler() instanceof WMBusAdapter)
+            .collect(Collectors.toList());
+    }
+
+    private void inject(WMBusAdapter adapter, String frame, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        byte[] bytes = HexUtils.hexToBytes(frame.replace(" ", ""));
+        try {
+            WMBusMessage message = VirtualWMBusMessageHelper.decode(bytes, 0, Collections.emptyMap());
+            WMBusDevice device = new WMBusDevice(message, adapter);
+            adapter.processMessage(device);
+        } catch (DecodingException e) {
+            req.setAttribute("error", e);
+        }
+    }
+
+    private String getAdapters() {
+        String options = adapters().stream()
+                .map(adapter -> "<option value=\"" + adapter.getUID() + "\">" + adapter.getUID().getBridgeIds() + " " + adapter.getLabel() + "</option>")
+                .collect(Collectors.joining());
+        return options;
+    }
+
+    @Reference
+    protected void setThingManager(ThingRegistry thingRegistry) {
+        this.thingRegistry = thingRegistry;
+    }
+    @Reference
+    public void setHttpService(HttpService httpService) {
+        this.httpService = httpService;
+    }
+
+    @Activate
+    public void activate() throws ServletException, NamespaceException {
+        httpService.registerServlet("/wmbus", this, new Hashtable(), null);
+    }
+
+    @Deactivate
+    public void deactivate() {
+        httpService.unregister("/wmbus");
+    }
+
+}
