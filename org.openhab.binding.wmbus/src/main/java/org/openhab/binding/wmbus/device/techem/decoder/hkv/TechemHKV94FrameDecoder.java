0ea956a0fabbb37ac0ea4bea4bdd7720b28c91a9
Hanno - Felix Wagner <h.wagner@kugu-home.com>
2019-08-27 13:50:50 +0000

    Try to fix the frame decoding

--- org.openhab.binding.wmbus/src/main/java/org/openhab/binding/wmbus/device/techem/decoder/hkv/TechemHKV94FrameDecoder.java
+++ org.openhab.binding.wmbus/src/main/java/org/openhab/binding/wmbus/device/techem/decoder/hkv/TechemHKV94FrameDecoder.java
@@ -15,4 +15,37 @@ public class TechemHKV94FrameDecoder extends AbstractTechemHKVFrameDecoder {
     public TechemHKV94FrameDecoder() {
         super(TechemBindingConstants._68TCH148255_8, true);
     }
+
+    @Override
+    protected TechemHeatCostAllocator decode(WMBusDevice device, SecondaryAddress address, byte[] buffer) {
+        int offset = address.asByteArray().length + 2;
+        int coding = buffer[offset] & 0xFF;
+
+        if (coding == 0xA2) {
+            LocalDateTime lastReading = parseLastDate(buffer, offset + 2);
+            float lastValue = parseBigEndianInt(buffer, offset + 4);
+            LocalDateTime currentDate = parseCurrentDate(buffer, offset + 6);
+            float currentValue = parseBigEndianInt(buffer, offset + 10);
+
+            List<Record<?>> records = new ArrayList<>();
+            records.add(new Record<>(Record.Type.CURRENT_VOLUME, currentValue));
+            records.add(new Record<>(Record.Type.CURRENT_READING_DATE, currentDate));
+            records.add(new Record<>(Record.Type.PAST_VOLUME, lastValue));
+            records.add(new Record<>(Record.Type.PAST_READING_DATE, lastReading));
+            records.add(new Record<>(Record.Type.RSSI, device.getOriginalMessage().getRssi()));
+
+            if (reportsTemperature) {
+                float temp1 = parseTemperature(buffer, offset + 12);
+                float temp2 = parseTemperature(buffer, offset + 14);
+                records.add(new Record<>(Record.Type.ROOM_TEMPERATURE, Quantities.getQuantity(temp1, SIUnits.CELSIUS)));
+                records.add(
+                        new Record<>(Record.Type.RADIATOR_TEMPERATURE, Quantities.getQuantity(temp2, SIUnits.CELSIUS)));
+            }
+
+            return new TechemHeatCostAllocator(device.getOriginalMessage(), device.getAdapter(), records);
+        }
+
+        return null;
+    }
+
 }
